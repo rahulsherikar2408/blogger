fa17ff81f3533f23fed3aeeeb82caa3f2d64b075
rahulsherikar2408 <sherikarrahul59@gmail.com>
2026-07-07 11:42:20 +0530

    Instantiating Express and EJS

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 8000;
+
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
