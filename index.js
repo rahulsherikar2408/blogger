32cc4d7619b762b92190fbfbcfd33e827aa527a0
rahulsherikar2408 <sherikarrahul59@gmail.com>
2026-07-07 13:41:44 +0530

    Setting views Folder

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
