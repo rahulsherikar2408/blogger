b5bae601783e95fe47989ef37aa6a7efc40b422a
rahulsherikar2408 <sherikarrahul59@gmail.com>
2026-07-08 23:20:26 +0530

    Installing and setting dotenv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+import 'dotenv/config';
+
 import express from "express";
 import path from "path";
 
 import userRoute from "./routes/user.js";
 
 const app = express();
-const PORT = 8000;
+const port = process.env.PORT || 8000;
 
 // View Engine
 app.set("view engine", "ejs");
@@ -17,4 +19,4 @@ app.get('/', (req, res) => {
 
 app.use('/user', userRoute);
 
-app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+app.listen(port, () => console.log(`Server started at PORT: ${port}`));
