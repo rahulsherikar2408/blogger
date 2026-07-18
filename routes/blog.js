1910935fcec79a9709147c56f8bb6d2b9e8b06d2
rahulsherikar2408 <sherikarrahul59@gmail.com>
2026-07-18 21:22:25 +0530

    Removing previous code for uploading image locally

--- routes/blog.js
+++ routes/blog.js
@@ -1,30 +1,10 @@
 import {Router} from "express";
-import multer from "multer";
 import path from "path";
 import fs from "fs";
 
 import Blog from "../models/blog.js"
 import Comment from "../models/comment.js";
 import upload from "../middlewares/cloudinaryUpload.js";
-
-// Multer Disk Storage for uploading Coverimage
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb){
-//         const uploadPath = path.resolve(
-//             `./public/uploads/${req.user._id}`
-//         );
-
-//         fs.mkdirSync(uploadPath, { recursive: true });
-
-//         cb(null, uploadPath);
-//     },
-//     filename: function(req, file, cb){
-//         const fileName = `${Date.now()}-${file.originalname}`;
-//         cb(null, fileName);
-//     },
-// });
-
-// const upload = multer({storage: storage});
 
 const router = Router();
 
@@ -64,18 +44,6 @@ router.post('/comment/:blogId', async(req, res) => {
     });
     return res.redirect(`/blog/${req.params.blogId}`);
 });
-
-// Create Blog Route
-// router.post('/', upload.single("coverImage"), async(req, res) => {
-//     const {title, body } = req.body;
-//     const blog = await Blog.create({
-//         title: title,
-//         body: body,
-//         createdBy: req.user._id,
-//         coverImageURL: `/uploads/${req.user._id}/${req.file.filename}`,
-//     });
-//     return res.redirect(`/blog/${blog._id}`);
-// });
 
 router.post(
     "/",
